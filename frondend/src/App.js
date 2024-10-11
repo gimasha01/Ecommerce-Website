bd9fb62c07b2fc55497166d40c959ac9f212634b
gimasha01 <gimashagperera@gmail.com>
2024-10-11 21:20:07 +0530

    Created the pages files

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'; 
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      
       <Navbar/>
+      <Routes>
+        <Route  >
+      </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
